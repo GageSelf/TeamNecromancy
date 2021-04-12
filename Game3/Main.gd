d5e67b9a9f94dffbd736ff82f283c1ec75f84d78
GageSelf <57369721+GageSelf@users.noreply.github.com>
2021-04-12 04:59:25 -0500

    Update Main.gd

--- Game3/Main.gd
+++ Game3/Main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-export (PackedScene) var Obstacles
+export (PackedScene) var Mob
 var score
 
 func _ready():
@@ -23,26 +23,25 @@ func new_game():
 	$StartTimer.start()
 
 func _on_StartTimer_timeout():
-	$ObstacleTimer.start()
+	$MobTimer.start()
 	$ScoreTimer.start() # Replace with function body.
 
 
 func _on_ScoreTimer_timeout():
 	score += 1 # Replace with function body.
 
-
-func _on_ObstacleTimer_timeout():
-	$ObstaclePath/MobSpawnLocation.offset = randi()
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.offset = randi()
 	# Create a Mob instance and add it to the scene.
-	var obstacle = preload("Obstacles.gd").instance()
-	add_child(obstacle)
+	var mob = Mob.instance()
+	add_child(mob)
 	# Set the mob's direction perpendicular to the path direction.
-	var direction = $ObstaclePath/MobSpawnLocation.rotation + PI / 2
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
 	# Set the mob's position to a random location.
-	obstacle.position = $ObstaclePath/MobSpawnLocation.position
+	mob.position = $MobPath/MobSpawnLocation.position
 	# Add some randomness to the direction.
 	direction += rand_range(-PI / 4, PI / 4)
-	obstacle.rotation = direction
+	mob.rotation = direction
 	# Set the velocity (speed & direction).
-	obstacle.linear_velocity = Vector2(rand_range(obstacle.min_speed, obstacle.max_speed), 0)
-	obstacle.linear_velocity = obstacle.linear_velocity.rotated(direction) # Replace with function body.
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction) # Replace with function body.
